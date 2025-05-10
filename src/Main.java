b992155beb0b2b8aaa66454aa3f382cdb7814c4c
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-05-09 21:12:08 -0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<Integer> numbers = Arrays.asList(2,3,7,6,12,15);
+        List<Integer>evenNumbers = EvenNumberFilter.filterEvenNumbers(numbers);
+        System.out.println("Even numbers: " + evenNumbers);
     }
 }
